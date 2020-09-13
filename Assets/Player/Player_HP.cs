b342cbfc7a95f44a3f11c0dc14ca4cda1a98fea4
Ridenshi <n124.com60@gmail.com>
2020-09-13 12:41:02 +0400

    Начало положено!

---
+++ Assets/Player/Player_HP.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_HP : MonoBehaviour
+{
+    public int HP;
+    public int DPS;
+  
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Character"))
+        {
+            Debug.Log("смерт");
+        }
+      //  DestroyObject(GetType.Equals.name.Wall, 2f);
+    }
+}
---
+++ Assets/Scripts/Arrow_controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow_controller : MonoBehaviour
+{
+    public float speed;
+    private GameObject Arrow;
+    private Rigidbody2D rb;
+
+
+    private void Start()
+    {
+       // rb.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        
+        
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+          
+        
+        
+    }
+   
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Player")
+        {
+            
+            Stop();
+            Destroy(gameObject, 3f);
+
+
+        }
+    }
+    private void Stop()
+    {
+
+        speed = 0f;
+            //rb.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+
+    }
+}
