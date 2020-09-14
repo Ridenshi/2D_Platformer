be567a08bc2bf349963fe70b691b3fe400c08ce2
Ridenshi <n124.com60@gmail.com>
2020-09-14 20:48:50 +0400

    Добавил анимацию персонажу, пофиксил стрелу(фаербол)

---
+++ 2D Game Platformer/Assets/Scripts/Arrow_controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow_controller : MonoBehaviour
+{
+    public float speed;
+    private GameObject Arrow;
+    private Rigidbody2D rb;
+
+
+    private void Start()
+    {
+      // rb.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        
+        
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+          
+        
+        
+    }
+   
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Player")
+        {
+            
+            Stop();
+            Destroy(gameObject, 3f);
+
+
+        }
+    }
+    private void Stop()
+    {
+
+        speed = 0f;
+        //  rb.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+       
+
+    }
+}
